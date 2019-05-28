26745adea5214cc57cfbae50bd30afb6177562e3
Shiba Das <shiba.chandro.das@gmail.com>
2019-05-28 17:26:40 +0600

    uploading complete files

---
+++ script.js
@@ -0,0 +1,23 @@
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
+
+const ChangeColor = () =>{
+
+		body.style.background = "linear-gradient(to right,"
+		 + color1.value 
+		 + " , " 
+		 + color2.value 
+		 + " )";
+
+
+}
+
+color1.addEventListener("input",ChangeColor);
+color2.addEventListener("input",ChangeColor);
+
+
+
+
+
